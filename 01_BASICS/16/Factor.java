94fa12599c24ee241971e7b27898cdc98a9e88a8
pratikmaity00 <pratikmaity27910@gmail.com>
2026-03-02 16:25:20 +0530

    done

---
+++ 01_BASICS/16/Factor.java
@@ -0,0 +1,10 @@
+
+public class Factor {
+    public static void main(String[] args) {
+        int n = 10;
+        for(int i = 1; i<=10; i++) {
+            if(n%i == 0)
+                System.out.println(i);
+        }
+    }
+}
