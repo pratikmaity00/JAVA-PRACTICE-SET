4300145b6bb8643cd6814ad5bdb2f2744427db32
pratikmaity00 <pratikmaity27910@gmail.com>
2026-03-02 12:41:03 +0530

    done

---
+++ 01_BASICS/02/EvenOdd.java
@@ -0,0 +1,11 @@
+
+// EVEN OR ODD
+public class EvenOdd {
+    public static void main(String[] args) {
+        int n = 4;
+        if(n%2 == 0)
+            System.out.println(n + " is Even.");
+        else 
+            System.out.println(n + " is Odd.");
+    }
+}
