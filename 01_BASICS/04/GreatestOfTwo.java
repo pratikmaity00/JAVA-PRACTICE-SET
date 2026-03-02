73ff64d68f5ac363ecc58302eaa03d6efc0b5369
pratikmaity00 <pratikmaity27910@gmail.com>
2026-03-02 13:05:30 +0530

    done

---
+++ 01_BASICS/04/GreatestOfTwo.java
@@ -0,0 +1,9 @@
+class GreatestOfTwo {
+    public static void main(String[] args) {
+        int n1 = 10,  n2 = 20;
+        if(n1>n2)
+            System.out.println(n1 + " is greatest.");
+        else
+            System.out.println(n2 + " is greatest.");
+    }
+}
