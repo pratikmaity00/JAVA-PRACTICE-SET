e3e47ea4ff41fd8a7290a4c3399f47187075132b
pratikmaity00 <pratikmaity27910@gmail.com>
2026-03-02 16:12:14 +0530

    done

---
+++ 01_BASICS/15/Power.java
@@ -0,0 +1,8 @@
+
+public class Power {
+    public static void main(String[] args) {
+        int base = 2, exponent= 3;
+        int ans = (int) Math.pow(base, exponent);
+        System.out.println(ans);
+    }
+}
