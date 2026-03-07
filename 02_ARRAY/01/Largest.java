fc752c795ee2d7f96df0801997715d8d101853e3
pratikmaity00 <pratikmaity27910@gmail.com>
2026-03-07 10:09:44 +0530

    done

---
+++ 02_ARRAY/01/Largest.java
@@ -0,0 +1,14 @@
+// Find the largest element in an array
+
+public class Largest {
+    public static void main(String[] args) {
+        int[] arr = {4, 7, 1, 9, 3};
+        int max = arr[0];
+        for(int i = 0; i<arr.length; i++) {
+            if(arr[i] > max){
+                max = arr[i];
+            }
+        }
+        System.out.println(max);
+    }
+}
